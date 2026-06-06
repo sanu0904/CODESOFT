6656198ef6c8e417568c2b3a6d0af64b4958a3f1
sanu0904 <Sushantsrivastava0904@gmail.com>
2026-06-06 11:33:00 +0530

    Refactor chatbot responses and variable names

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-# Simple Rule-Based Chatbot
-# Chatbot Name: Alex
+#chatbot-ALEX
 
+ 
 print("Alex: Hello! I am Alex, your chatbot.")
 print("Alex: Type 'bye alex' if you want to end the chat.\n")
 
 while True:
 
-    # Taking input from user\name of user 
-    user_message = input("SUSHANT: ").lower()
+    user_input = input("SUSHANT: ").lower()
 
-    # Greeting responses
     if user_message == "hello alex" or user_message == "hi alex":
+
         print("Alex: Hi! Nice to meet you.")
 
-    # Asking chatbot name
     elif "what is your name" in user_message:
         print(" My name is Alex.")
 
-    # Asking about chatbot
     elif "how are you" in user_message:
-        print("Alex: I am doing great. Thanks for asking!")
+        print("Alex: I am fine. Thanks for asking!")
 
-    # Asking time
     elif "who made you" in user_message:
         print("Alex: my devloper is sushant sir")
 
-    # Asking about Python
     elif "what is python" in user_message:
         print("Alex: Python is an easy and powerful programming language.")
 
-    # Goodbye message
     elif user_message == "bye alex":
-        print("Alex: Goodbye! Have a wonderful day.")
+        print("Alex: Bye mate! Have a wonderful day.")
         break
 
-    # Default response
     else:
-        print("Alex: Sorry, I did not understand that.")
+        print("Alex: Sorry i am still at learning phase.")
